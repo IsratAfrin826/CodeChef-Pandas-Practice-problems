23d773c43ec667fe65d8b0176d432f7271477f7d
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-07-02 14:12:05 +0600

    Add files via upload

---
+++ Create Series from List.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Given list of strings
+data_list = ["apple", "banana", "cherry", "date", "elderberry"]
+
+# Create a Pandas Series from data_list
+my_series = pd.Series(data_list)
+
+# Print the created Series
+print(my_series)
